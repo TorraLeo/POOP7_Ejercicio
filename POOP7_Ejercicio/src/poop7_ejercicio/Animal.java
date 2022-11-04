ec37679b8e4456abc74e4027e40d8cc85b255bc5
TorraLeo <103598865+TorraLeo@users.noreply.github.com>
2022-11-03 22:12:54 -0600

    Cambios Animal

--- POOP7_Ejercicio/src/poop7_ejercicio/Animal.java
+++ POOP7_Ejercicio/src/poop7_ejercicio/Animal.java
@@ -1,5 +1,89 @@
 package poop7_ejercicio;
 
 public class Animal {
+    String nombre;
+    String lugarOrigen;
+    String color;
+    String sonido;
     
+    public Animal(){
+
+    }
+    
+    public Animal(String nombre, String lugarOrigen, String color) {
+        this.nombre = nombre;
+        this.lugarOrigen = lugarOrigen;
+        this.color = color;
+    }
+
+    /**
+     * @return Regresa el nombre del animal
+     * 
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return Regresa el lugar de origen
+     */
+    public String getLugarOrigen() {
+        return lugarOrigen;
+    }
+
+    /**
+     * @return Regresa el color del animal
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param nombre Método que modifica el nombre del animal
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @param lugarOrigen Método que modifica el lugar de origen
+     */
+    public void setLugarOrigen(String lugarOrigen) {
+        this.lugarOrigen = lugarOrigen;
+    }
+
+    /**
+     * @param color Método que modifica el color
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void comer(){
+        System.out.println(getNombre() + " esta comiendo");
+    }
+
+    @Override
+    public String toString() {
+        return "Animal [nombre=" + nombre + ", lugarOrigen=" + lugarOrigen + ", color=" + color + "] ";
+    }
+
+    /**
+     * @return Regresa el sonido del animal
+     */
+    public String getSonido() {
+        return sonido;
+    }
+
+    /**
+     * @param sonido Método que modifica el sonido del animal
+     */
+    public void setSonido(String sonido) {
+        this.sonido = sonido;
+    }
+
+    public void sonido(){
+        System.out.println(getSonido());
+    }
+
 }
--- POOP7_Ejercicio/src/poop7_ejercicio/POOP7_Ejercicio.java
+++ POOP7_Ejercicio/src/poop7_ejercicio/POOP7_Ejercicio.java
@@ -14,7 +14,7 @@ public class POOP7_Ejercicio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
