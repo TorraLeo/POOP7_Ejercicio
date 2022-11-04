a4d20de5d1071ae2a53ee2c54e46e2e523a47d1f
TorraLeo <103598865+TorraLeo@users.noreply.github.com>
2022-11-03 22:16:16 -0600

    Cambios Perro

--- POOP7_Ejercicio/src/poop7_ejercicio/Perro.java
+++ POOP7_Ejercicio/src/poop7_ejercicio/Perro.java
@@ -1,5 +1,35 @@
 package poop7_ejercicio;
 
 public class Perro extends AnimalTerrestre {
+    String colorCollar;
     
+    public Perro(String nombre, String lugarOrigen, String color, int numeroPatas, String colorCollar) {
+        super(nombre, lugarOrigen, color, numeroPatas);
+        this.colorCollar=colorCollar;
+    }
+    
+    public Perro() {
+    }
+
+    /**
+     * @return Regresa el color del collar del perro
+     */
+    public String getColorCollar() {
+        return colorCollar;
+    }
+    /**
+     * @param colorCollar Método que modifica el color del collar
+     */
+    public void setColorCollar(String colorCollar) {
+        this.colorCollar = colorCollar;
+    }
+
+    public void hacerTruco(){
+        System.out.println(getNombre()+"truco");
+    }
+
+    @Override
+    public String toString() {
+        return "Perro -> " + super.toString()+ " [colorCollar=" + colorCollar + "] ";
+    }
 }
