a442d8d60461419e6d5da5fba3bfe38f4341b5c3
TorraLeo <103598865+TorraLeo@users.noreply.github.com>
2022-11-03 22:19:45 -0600

    Cambios AnimalAereo

--- POOP7_Ejercicio/src/poop7_ejercicio/AnimalAereo.java
+++ POOP7_Ejercicio/src/poop7_ejercicio/AnimalAereo.java
@@ -1,5 +1,36 @@
 package poop7_ejercicio;
 
 public class AnimalAereo extends Animal {
-    
+    int numeroAlas;
+
+    public AnimalAereo(String nombre, String lugarOrigen, String color, int numeroAlas) {
+        super(nombre, lugarOrigen, color);
+        this.numeroAlas = numeroAlas;
+    }
+
+    public AnimalAereo() {
+    }
+
+    /**
+     * @return Regresa el numero de alas
+     */
+    public int getNumeroAlas() {
+        return numeroAlas;
+    }
+
+    /**
+     * @param numeroAlas Método que modifica el numero de alas
+     */
+    public void setNumeroAlas(int numeroAlas) {
+        this.numeroAlas = numeroAlas;
+    }
+
+    public void volar(){
+        System.out.println(getNombre() + " esta volando");
+    }
+
+    @Override
+    public String toString() {
+        return "AnimalAereo -> " + super.toString()+ " [numeroAlas=" + numeroAlas + "] ";
+    }
 }
